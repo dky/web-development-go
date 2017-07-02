f4fc02cca38ab8dd78764a7f4315808b62c4284f
Don Ky <don.d.ky@gmail.com>
2017-07-02 16:05:00 -0400

    Working example of 6, add fresh runner.conf

--- 6/main.go
+++ 6/main.go
@@ -10,24 +10,29 @@ import (
 
 var homeTemplate *template.Template
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	homeTemplate.Execute(w, nil)
-}
-
-func contact(w http.ResponseWriter, r http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email " +
-	"to <a href=\"mailto:support@usegolang.com\">" +
-	"support@usegolang.com</a>.")
-}
 
 func main() {
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
 
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	homeTemplate.Execute(w, nil)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email " +
+	"to <a href=\"mailto:support@usegolang.com\">" +
+	"support@usegolang.com</a>.")
 }

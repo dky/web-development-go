a24bb0ef8ca525f2b5e93cd98f806ab2467da8b6
Don Ky <don.d.ky@gmail.com>
2017-07-06 12:33:53 -0400

    Start work on chapter 7

---
+++ 7/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"./views"
+
+	"github.com/gorilla/mux"
+)
+
+var homeView *views.View 
+var contactView *views.View
+var faqsView *views.View
+
+func main() {
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	faqsView = views.NewView("bootstrap", "views/faqs.gohtml")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faqs", faqs)
+	http.ListenAndServe(":3000", r)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Println(homeView.Layout)
+	//homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
+	homeView.Render(w, nil)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	//contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
+	contactView.Render(w, nil)
+}
+
+func faqs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	faqsView.Render(w, nil)
+}
---
+++ 7/views/views.go
@@ -0,0 +1,47 @@
+package views
+
+import (
+	"net/http"
+	"html/template"
+	"path/filepath"
+)
+
+var LayoutDir string = "views/layouts"
+
+//func NewView(files ...string) *View {
+func NewView(layout string, files ...string) *View {
+	files = append(files, layoutFiles()...)
+	/*
+	files = append(files, 
+		"views/layouts/footer.gohtml",
+		"views/layouts/navbar.gohtml",
+		"views/layouts/bootstrap.gohtml")
+	*/
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+
+	return &View {
+		Template: t,
+		Layout: layout,
+	}
+}
+
+type View struct {
+	Template *template.Template
+	Layout string
+}
+
+//zero args, returns a slice of strings = files that match "/*.gohtml"
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "/*.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
